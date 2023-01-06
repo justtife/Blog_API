da00ab2f344d8f637675345a02d6d1f54f00c49e
Justtife <farinubolu@gmail.com>
2023-01-06 08:24:35 +0100

    Changed the logger to winston

--- db/connect.js
+++ db/connect.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
+const logger = require("../logger/index");
 
 const connectDB = async (url) => {
   try {
     //Connect to database
     await mongoose.connect(url);
-    console.log(`Database Connected`);
+    logger.info(`Database Connected`);
   } catch (err) {
     //Return Error if error exist
-    console.error(`Database Error: ${err}`);
+    logger.error(`Database Error: ${err}`);
     process.exit(1);
   }
 };
